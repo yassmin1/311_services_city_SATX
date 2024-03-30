47b8c0416f635ad2d597c29c7293cc1d14ac7d6a
Rayan Yassminh <38767315+yassmin1@users.noreply.github.com>
2024-03-30 14:53:48 -0600

    Update predict_model.py

---
+++ 311_services_city_SATX/src/models/predict_model.py
@@ -0,0 +1,15 @@
+import joblib
+import pandas as pd
+#
+X_test=pd.read_csv('../data/X_test')
+
+#
+chosen_threshold =0.14
+# Load the model from the file
+###
+model=joblib.load(model, 'model.pkl')
+y_pred_proba_test = model.predict_proba(X_test)[:, 1]
+# 
+predicted_classes = (y_pred_proba_test >= chosen_threshold).astype(int)
+print(confusion_matrix(y_test,predicted_classes))
+print(classification_report(y_test,predicted_classes))
